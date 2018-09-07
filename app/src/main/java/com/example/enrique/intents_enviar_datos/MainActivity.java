e3eebb92080322c2420fe78bff0f98271f2027e0
Enrique <Enrique@DESKTOP-5D0H3DI>
2018-09-06 20:09:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/enrique/intents_enviar_datos/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.enrique.intents_enviar_datos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void establecerMenu(boolean xSwitch) {
+
+    }
+}
